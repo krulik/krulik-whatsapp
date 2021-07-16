9cf5a57bc2cbd96004bfd426f5234181fdcd8c8d
krulik <iwillkissyou@gmail.com>
2021-07-16 11:21:04 +0300

    another url

--- src/utils.js
+++ src/utils.js
@@ -5,12 +5,14 @@ export function getChatUsersList(chat, usersContext) {
   }).join(', ');
 }
 
-export let get = (route) => fetch(`http://localhost:8080/api/${route}`, {
+const API_URL = 'https://whatsapp-server-zlilyoffe.herokuapp.com';
+
+export let get = (route) => fetch(`${API_URL}/${route}`, {
   credentials: 'include',
   mode: 'cors'
 }).then(res => res.json())
 
-export let post = (route, body) => fetch(`http://localhost:8080/api/${route}`, {
+export let post = (route, body) => fetch(`${API_URL}/${route}`, {
   method: 'POST',
   headers: {
     'Accept': 'application/json',
